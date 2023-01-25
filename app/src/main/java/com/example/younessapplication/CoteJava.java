37e4fc05df2cb49f8e53a2bcdd77fde862da98bf
youness elhakimy <elhakimyyouness4@gmail.com>
2023-01-26 00:34:14 +0100

    les buttons qtt et rein ajouter dans la cote java

--- app/src/main/java/com/example/younessapplication/CoteJava.java
+++ app/src/main/java/com/example/younessapplication/CoteJava.java
@@ -3,12 +3,36 @@ package com.example.younessapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CoteJava extends AppCompatActivity {
+    Button btnre,btnqtt;
+    EditText Number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cote_xml);
+        btnre=findViewById(R.id.btn_reinitialiser);
+        btnqtt=findViewById(R.id.btn_quitter);
+        Number=findViewById(R.id.TextNum);
+
+        btnre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Number.setText(null);
+            }
+        });
+
+        btnqtt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+        
     }
 }
