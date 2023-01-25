43500ad8f2611de2a0dc529955dcd70dc1f0d2d8
youness elhakimy <elhakimyyouness4@gmail.com>
2023-01-26 00:21:42 +0100

    commit initial

---
+++ app/src/main/java/com/example/younessapplication/CoteJava.java
@@ -0,0 +1,14 @@
+package com.example.younessapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoteJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cote_xml);
+    }
+}
