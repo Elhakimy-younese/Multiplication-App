ec619c8026a49b17224f483f2368bef23f028401
youness elhakimy <elhakimyyouness4@gmail.com>
2023-01-26 01:18:42 +0100

    bug fixee

--- app/src/main/java/com/example/younessapplication/CoteJava.java
+++ app/src/main/java/com/example/younessapplication/CoteJava.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class CoteJava extends AppCompatActivity {
     Button btnre,btnqtt;
     EditText Number;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,13 @@ public class CoteJava extends AppCompatActivity {
         btnre=findViewById(R.id.btn_reinitialiser);
         btnqtt=findViewById(R.id.btn_quitter);
         Number=findViewById(R.id.TextNum);
+        tv=findViewById(R.id.txtv);
 
         btnre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Number.setText(null);
+                tv.setText("? * 1 =?\n ? * 2 =?\n ? * 3 =?\n ? * 4 =?\n ? * 5 =?\n ? * 6 =?\n ? * 7 =?\n ? * 8 =?\n ? * 9 =?\n ? * 10 =?");
             }
         });
 
